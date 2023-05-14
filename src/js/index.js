17d6d6b9967c20331c9355eb9e260464f1520797
Joao-VictorCm <129808291+Joao-VictorCm@users.noreply.github.com>
2023-05-13 23:49:50 -0300

    Add files via upload

---
+++ src/js/index.js
@@ -0,0 +1,20 @@
+const botaoAlterarTema = document.getElementById("botao-alterar-tema");
+
+const body = document.querySelector("body");
+const imgBotaoTrocaDeTema = document.querySelector(".imagem-botao");
+
+botaoAlterarTema.addEventListener('click', () => {
+    const modoEscuroEstaAtivo = body.classList.contains("modo-escuro");
+
+    body.classList.toggle("modo-escuro")
+
+    if (modoEscuroEstaAtivo) {
+
+        imgBotaoTrocaDeTema.setAttribute("src", "./src/imagens/sun.png");
+    }
+    else {
+
+        imgBotaoTrocaDeTema.setAttribute("src", "./src/imagens/moon.png");
+    }
+
+});
